fc016e90156e2e6ffc05a371607429ed7d95cf38
Alex <tucanae@icloud.com>
2025-05-09 15:48:00 +0100

    Initial Commit

---
+++ GitHubViewer/GitHubViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubViewerApp.swift
+//  GitHubViewer
+//
+//  Created by Alex on 09/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct GitHubViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
